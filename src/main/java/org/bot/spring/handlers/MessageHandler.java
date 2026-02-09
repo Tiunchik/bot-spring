b7251352692a74ac1103668350504fd0de796d66
PC-Max <senebh@gmail.com>
2026-02-09 23:02:43 +0300

    ADD TODOs

--- src/main/java/org/bot/spring/handlers/MessageHandler.java
+++ src/main/java/org/bot/spring/handlers/MessageHandler.java
@@ -2,6 +2,7 @@ package org.bot.spring.handlers;
 
 import org.bot.spring.dto.MessageContext;
 
+//TODO: добавить тикток
 public interface MessageHandler {
     boolean canHandle(String text);
     void handle(String text, MessageContext context);
